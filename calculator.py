117ef44f80b32a5f15a38c4fde9eb2b1bc57197f
Diego <diegopenuelagonzalez@gmail.com>
2025-04-09 16:59:49 -0400

    Added 3 new functions

--- calculator.py
+++ calculator.py
@@ -6,7 +6,15 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def add(a, b): 
+def square_root(a):
+    if a < 0:
+        raise ValueError
+    return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
+def add(a, b):
     return a + b
 
 def subtract(a, b):
